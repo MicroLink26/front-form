597868ee5e2b9aac871d42e0fe48cb417193639c
Mike <mic.roche@gmail.com>
2023-06-17 11:27:10 +0200

    New commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,97 +1,109 @@
-// ===> Création du variable pour ne pas avoir besoin de répéter "document" à chaque fois
 const $ = document;
 
-// ==> INDISPENSABLE == écoute si le contenue de la page  est bien chargée
-// Tout doit être dans la fonction de cet écouteur
 $.addEventListener("DOMContentLoaded", () => {
-  console.log("content loaded");
+  const modal = $.querySelector(".modal");
+  const body = $.querySelector("body");
+  const header = $.querySelector("header");
+  const form = $.querySelector("#contact-form");
+  const submitButton = $.querySelector("#submit-btn");
 
-  let updateButton = document.getElementById("updateDetails");
-  let favDialog = document.getElementById("favDialog");
-  // let outputBox = document.querySelector("output");
-  // let selectEl = document.querySelector("select");
-  // let confirmBtn = document.getElementById("confirmBtn");
-
-  // Le bouton "Mettre à jour les détails" ouvre le <dialogue> ; modulaire
-  updateButton.addEventListener("click", function onOpen() {
-    if (typeof favDialog.showModal === "function") {
-      favDialog.showModal();
+  // ----------------------------------------
+  // Add bottom border on header when scrolling on axe Y
+  // ----------------------------------------
+  window.addEventListener("scroll", () => {
+    let scroll = window.scrollY;
+    if (scroll !== 0) {
+      header.classList.add("bordered");
     } else {
-      console.error(
-        "L'API <dialog> n'est pas prise en charge par ce navigateur."
-      );
+      header.classList.remove("bordered");
     }
   });
-  // L'entrée "Animal favori" définit la valeur du bouton d'envoi.
-  // selectEl.addEventListener("change", function onSelect(e) {
-  //   confirmBtn.value = selectEl.value;
-  // });
-  // Le bouton "Confirmer" du formulaire déclenche la fermeture
-  // de la boîte de dialogue en raison de [method="dialog"]
-  $.querySelector(".close").addEventListener("click", () => {
-    favDialog.close();
+
+  // ----------------------------------------
+  // When the button "Connectez-vous" is clicked
+  // ----------------------------------------
+  $.querySelector(".btn-signup").addEventListener("click", () => {
+    // Display modal
+    modal.classList.remove("hidden");
+    // Disable scroll
+    body.classList.add("disable");
   });
-  // ===> Ecoute un clic n'importe où sur la page
-  //   document.addEventListener("click", () => {
-  //     console.log("clicked");
-  //   });
 
-  //   ===> Au clic sur le bouton Hide, le texte disparait
-  // $.querySelector("#hide").addEventListener("click", () => {
-  //   console.log("hide");
+  // // ----------------------------------------
+  // // Submit the form (=> when the back is running)
+  // // ----------------------------------------
+  // // Function to empty the form
+  const cleanForm = () => {
+    form.reset();
+  };
 
-  //   $.querySelector("p").classList.add("hidden");
-  // });
+  // // Function to deactivate the submit button
+  const isDisabled = () => {
+    submitButton.setAttribute("disabled", "disabled");
+    submitButton.classList.add("disabled-btn");
+  };
 
-  //   ===> Au clic sur le bouton Display, le texte apparait
-  // $.querySelector("#display").addEventListener("click", () => {
-  //   console.log("display");
+  // // Function to reactivate the submit button
+  const isEnabled = () => {
+    submitButton.removeAttribute("disabled");
+    submitButton.classList.remove("disabled-btn");
+  };
 
-  //   $.querySelector("p").classList.remove("hidden");
-  // });
-
-  //  ===> Au clic sur le bouton Hide/Display, le texte disparait/apparait
-  // $.querySelector("#hide-display").addEventListener("click", () => {
-  //   console.log("hide display");
-
-  //   $.querySelector("p").classList.toggle("hidden");
-
-  //   const button = $.querySelector("#hide-display");
-
-  //   // console.log(button.textContent);
-
-  //   if (button.textContent === "Hide") {
-  //     button.textContent = "Display";
-  //   } else {
-  //     button.textContent = "Hide";
-  //   }
-  // });
-
-  //   ===> Déclenchement de la requête à la soumission du formulaire
-  document.querySelector("form").addEventListener("submit", async (event) => {
-    //   // ===> Empêche le comportement par défaut de la soumission d'un formulaire = rafraîchissement de la page
+  // // When the submit button is clicked
+  form.addEventListener("submit", async (event) => {
     event.preventDefault();
+    isDisabled();
 
-    //   // console.log("submit", event);
+    const data = {
+      firstname: $.querySelector("#firstname").value,
+      lastname: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      message: $.querySelector("#message").value,
+    };
 
-    //   // ===> On récupère les valeurs de chaque input
-    const firstname = document.querySelector("#firstname").value;
-    const lastname = document.querySelector("#lastname").value;
-    const message = document.querySelector("#message").value;
-    const email = document.querySelector("#email").value;
+    try {
+      const response = await axios.post(
+        "https://site--mailing--fg6zdpvl2w9z.code.run/form",
+        data
+      );
 
-    //   // console.log({ firstname, lastname, email, message });
-
-    const response = await axios.post(
-      "https://site--mailing--fg6zdpvl2w9z.code.run/form",
-      {
-        firstname,
-        lastname,
-        email,
-        message,
+      if (response.status === 200) {
+        alert("Votre formulaire a bien été envoyé");
+        cleanForm();
+        isEnabled();
       }
-    );
-    favDialog.close();
-    //   console.log(response.data);
+    } catch (error) {
+      if (error.response?.data.message === "Missing parameters") {
+        alert("Veuillez remplir tous les champs du formulaire");
+      } else {
+        alert("Une erreur est survenue");
+        cleanForm();
+      }
+
+      isEnabled();
+    }
+  });
+
+  // ----------------------------------------
+  // When the cross on the modal is clicked
+  // ----------------------------------------
+  $.querySelector(".icon-times").addEventListener("click", () => {
+    // Hide modal
+    modal.classList.add("hidden");
+    // Enable scroll
+    body.classList.remove("disable");
+  });
+
+  // ----------------------------------------
+  // Easter Egg
+  // ----------------------------------------
+  const easterEgg = $.querySelector("#surprise");
+  easterEgg.addEventListener("click", () => {
+    const attributeValue = easterEgg.getAttribute("src");
+    if (attributeValue === "./assets/img/favicon-tripadvisor.svg") {
+      easterEgg.setAttribute("src", "./assets/img/logo.png");
+    } else {
+      easterEgg.setAttribute("src", "./assets/img/favicon-tripadvisor.svg");
+    }
   });
 });
